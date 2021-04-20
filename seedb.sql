20d908f291a6bb867b3c72bcb149fc3af09c93f0
dgordonf <46106765+dgordonf@users.noreply.github.com>
2021-04-19 21:51:05 -0400

    init

--- seedb.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM search_index WHERE article_id = 'R_dc2ae0036b665a4277b784de5f6f19aa';
-
-DELETE FROM sentences WHERE article_id = 'R_dc2ae0036b665a4277b784de5f6f19aa';
-DELETE FROM search_index WHERE article_id = 'R_dc2ae0036b665a4277b784de5f6f19aa';
-
-SELECT * FROM sentences WHERE article_id = 'R_dc2ae0036b665a4277b784de5f6f19aa';
-
-DELETE FROM sentences WHERE author_id < 1;
-DELETE FROM search_index WHERE author_id < 1;
