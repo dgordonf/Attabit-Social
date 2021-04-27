219d6b6bd907c2cb4746cb6c5067d393c0788a0d
dgordonf <46106765+dgordonf@users.noreply.github.com>
2021-04-27 07:41:52 -0400

    update

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import re
 from flask_gtts import gtts
 from config import Config
 from flask_login import LoginManager
+import flask_bcrypt
 
 # Not the entire world, just your best friends. 
 app = Flask(__name__)
@@ -24,9 +25,41 @@ try:
 except:
     print ("I am unable to connect to the database")
 
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('index.html')  
+
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+    if request.method == 'POST':
+        try:
+            username = request.form.get('username')
+            email = request.form.get('email')
+            first_name = request.form.get('first_name')
+            last_name = request.form.get('last_name')
+            password_hash = request.form.get('password')
+            
+            connection.execute("INSERT INTO users (username, email, password_hash, first_name, last_name) VALUES (%s, %s, %s, %s, %s);", (username, email, password_hash, first_name, last_name))
+
+            return redirect("camp/1")    
+        
+        except Exception as e:
+            # e holds description of the error
+            error_text = "<p>The error:<br>" + str(e) + "</p>"
+            hed = '<h1>Something is broken.</h1>'
+            return hed + error_text 
+
+
+
+    return render_template('index.html')         
+
 
 @app.route('/camp/<int:camp_id>', methods = ['GET'])
 def camp(camp_id):
